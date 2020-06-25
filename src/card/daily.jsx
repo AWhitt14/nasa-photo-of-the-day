534d1af409b207996ae19b123ae9ced860652dda
Alex Whitt <whitta14@gmail.com>
2020-06-25 16:59:05 -0400

    finished some styling

--- src/card/daily.jsx
+++ src/card/daily.jsx
@@ -1,22 +1,32 @@
 import React, { useState } from "react";
-
-import { Button, Fade, Alert } from 'reactstrap'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import styled from "styled-components";
+import { Button, Fade } from 'reactstrap';
 
 const Daily = ({ title, date, explanation, url }) => {
-  const cont = styled.p`
-    margin: 20%;
-    border: 2px solid dodgerblue;
-  `;
+  const [fadeIn, setFadeIn] = useState(true);
 
+  const toggle = () => {setFadeIn(!fadeIn)
+  };
+
+  const Wrapper = styled.section`
+  padding: 1rem;
+  background: lightblue;
+  margin: 10%;
+  margin-top: 1%;
+  border: 2px solid dodgerblue;
+`;
+  
   return (
     <div>
      <h2>{title} |</h2>
      <p>{date}</p>
-     <div>
+     <Wrapper>
       <img src={url} alt='img'/>
-      <p>{explanation}</p>
-     </div>
-     <Button color="succes">Info</Button>
+      <Fade in={fadeIn} tag="h5" className="mt-3">{explanation}</Fade>
+      <Button color="danger" onClick={toggle}>Toggle Fade</Button>
+     </Wrapper>
+     
     </div>
   );
 };
