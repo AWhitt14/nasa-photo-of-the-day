935b584e0999775f61f541fc1936e13e800b94f9
Alex Whitt <whitta14@gmail.com>
2020-06-25 16:33:10 -0400

    added reactstrap and styled

--- src/card/daily.jsx
+++ src/card/daily.jsx
@@ -1,15 +1,22 @@
-import React from "react";
-import './daily.css';
+import React, { useState } from "react";
+
+import { Button, Fade, Alert } from 'reactstrap'
 
 const Daily = ({ title, date, explanation, url }) => {
+  const cont = styled.p`
+    margin: 20%;
+    border: 2px solid dodgerblue;
+  `;
+
   return (
     <div>
      <h2>{title} |</h2>
-     <p class='date'>{date}</p>
-     <div class='imgBox'>
+     <p>{date}</p>
+     <div>
       <img src={url} alt='img'/>
       <p>{explanation}</p>
      </div>
+     <Button color="succes">Info</Button>
     </div>
   );
 };
